f517982d841ace66959c0026980e1386ea770b38
diegomarrufo <dmarrufo@ucm.es>
2022-07-07 01:34:35 +0200

    associations tests with rspec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Associations
+  it { should have_many(:labels)}
+  # Validation tests
+  pending "need to write validations!"
 end
